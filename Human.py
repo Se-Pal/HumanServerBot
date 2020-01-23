7ea4c92300c62e665a77da0c95cc88dffd369f13
Se-Pal <60204649+Se-Pal@users.noreply.github.com>
2020-01-23 14:34:21 +0900

    Add files via upload

---
+++ Human.py
@@ -0,0 +1,32 @@
+import discord
+
+client = discord.Client()
+async def on_ready():
+    print(client.user.id)
+    print("ready")
+    game = discord.Game("재밌는 휴먼팜")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith(".섭주"):
+        await message.channel.send("> ServerIP : HumanF.kro.kr")
+
+    if message.content.startswith(".A"):
+        await message.channel.send("> 🚧　제작중　🚧")
+
+    if message.content.startswith(".B"):
+        await message.channel.send("> 🚧　제작중　🚧")
+
+    if message.content.startswith(".C"):
+        await message.channel.send("> 🚧　제작중　🚧")
+
+    if message.content.startswith(".D"):
+        await message.channel.send("> 🚧　제작중　🚧")
+
+
+client.run("NjY4ODU4NDc4MDAwOTk2NDI3.XiXZCg.iElpNaXDxheAG7iSsGWeyL_hcGk")
